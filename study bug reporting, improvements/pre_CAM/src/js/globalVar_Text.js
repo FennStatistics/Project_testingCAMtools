429a42c3aa1ab010449e696a1778348163da2cea
Camel-app <camel.app@proton.me>
2024-04-04 10:39:35 +0200

    upload reporting study

--- study bug reporting, improvements/pre_CAM/src/js/globalVar_Text.js
+++ study bug reporting, improvements/pre_CAM/src/js/globalVar_Text.js
@@ -50,11 +50,12 @@ report a possible <b>improvement</b>
    <col style="width: 50%">
  </colgroup>
 </table>
-</form>
+
+
+
 
 
 <table id="type_bug" style="display: none">
-
 <!-- Remark -->
 <tr style="height: 100px">
   <td class="text-left" colspan="2">
@@ -68,7 +69,7 @@ report a possible <b>improvement</b>
   For which tool would you like to report a <b>bug</b>?
   </td>
   <td>
-    <select id="tool_bug" name="tool_bug" required class="w-100">
+    <select id="bug_tool" name="bug_tool" class="w-100">
       <option value="" selected>
       -- Please select --
       </option>
@@ -80,12 +81,215 @@ report a possible <b>improvement</b>
 </tr>
 
 
+<!-- bug CAMEL element -->
+<tr style="height: 100px" id="bug_part_CAMEL">
+  <td class="text-left">
+  For which <b>element (concept, connection, other)</b> would you like to report a bug?
+  </td>
+  <td>
+    <select id="bug_CAMEL_element" name="bug_CAMEL_element" class="w-100">
+      <option value="" selected>
+      -- Please select --
+      </option>
+      <option value="concept">Concept (ideas, goals drawn by participants).</option>
+      <option value="connection">Connection (connections drawn between concepts).</option>
+      <option value="other">Other.</option>
+    </select>
+  </td>
+</tr>
+
+
+<!-- bug CAMApp part -->
+<tr style="height: 100px" id="bug_part_CAMApp">
+  <td class="text-left">
+  For which <b>part (upload, preprocessing, analysis)</b> would you like to report a bug?
+  </td>
+  <td>
+    <select id="bug_CAMApp_part" name="bug_CAMApp_part" class="w-100">
+      <option value="" selected>
+      -- Please select --
+      </option>
+      <option value="upload">Upload Part (when you enter the CAM-App).</option>
+      <option value="preprocessing">Preprocessing Part (when you clicked on the respective button).</option>
+      <option value="analysis">Analysis Part (when you clicked on the respective button).</option>
+    </select>
+  </td>
+</tr>
+
+
+<!-- bug Webpage part -->
+<tr style="height: 100px" id="bug_part_Webpage">
+  <td class="text-left">
+  For which <b>part (before log in, after log in, other)</b> of the webpage (administrative panel) would you like to report a bug?
+  </td>
+  <td>
+    <select id="bug_Webpage_part" name="bug_Webpage_part" class="w-100">
+      <option value="" selected>
+      -- Please select --
+      </option>
+      <option value="beforeLogIn">Before log in (before you have logged in into your account).</option>
+      <option value="afterLogIn">After log in (after you have logged in into your account).</option>
+      <option value="other">Other.</option>
+    </select>
+  </td>
+</tr>
+
+
+<!-- bug browser used -->
+<tr style="height: 50px" class="bug_allOthers">
+  <td class="text-left">
+  <img src = "pre_CAM/static/browser icons/programmer-computer-icon.svg" alt="Browser Icon" width="50px;" heigth="50px";>
+  Which Browser do you have used?
+  </td>
+  <td>
+<div id="placeholder_browser">XXX</div>
+
+<input type="radio" name="bug_browser_used" value="right" id="browserUsed">
+<label for="browserUsed" class="text-left">
+✔ Right
+</label>
+<br>
+<input type="radio" name="bug_browser_used" value="wrong" id="browserUsed2">
+<label for="browserUsed2" class="text-left">
+✘ Wrong <span style="font-size: 14px;">(I encountered the issue on a different device or browser)</span>
+</label>
+  </td>
+</tr>
+
+
+
+<!-- bug additional browser used -->
+<tr style="height: 100px" class="bug_allOthers">
+  <td class="text-left" colspan="2">
+  Please select another browser you have tested on the bug you want to report.
+  <br>
+  
+  <input type="checkbox" id="Chrome" class="icon-checkbox">
+  <label for="Chrome" class="icon-label">
+    <img src = "pre_CAM/static/browser icons/browser-chrome-icon.svg" alt="" width="50px;" heigth="50px";>
+    <p style="margin-left: 5px;">Chrome</p>
+  </label>
+
+  <input type="checkbox" id="Edge" class="icon-checkbox">
+  <label for="Edge" class="icon-label">
+    <img src = "pre_CAM/static/browser icons/browser-edge-icon.svg" alt="" width="50px;" heigth="50px";>
+    <p style="margin-left: 5px;">Edge</p>
+  </label>
+
+  <input type="checkbox" id="Firefox" class="icon-checkbox">
+  <label for="Firefox" class="icon-label">
+    <img src = "pre_CAM/static/browser icons/browser-firefox-icon.svg" alt="" width="50px;" heigth="50px";>
+    <p style="margin-left: 5px;">Firefox</p>
+  </label>
+
+  <input type="checkbox" id="Internet Explorer" class="icon-checkbox">
+  <label for="Internet Explorer" class="icon-label">
+    <img src = "pre_CAM/static/browser icons/browser-internet-explorer-icon.svg" alt="" width="50px;" heigth="50px";>
+    <p style="margin-left: 5px;">Internet Explorer</p>
+  </label>
+
+  <input type="checkbox" id="Opera" class="icon-checkbox">
+  <label for="Opera" class="icon-label">
+    <img src = "pre_CAM/static/browser icons/browser-opera-icon.svg" alt="" width="50px;" heigth="50px";>
+    <p style="margin-left: 5px;">Opera</p>
+  </label>
+
+  <input type="checkbox" id="Safari" class="icon-checkbox">
+  <label for="Safari" class="icon-label">
+    <img src = "pre_CAM/static/browser icons/browser-safari-icon.svg" alt="" width="50px;" heigth="50px";>
+    <p style="margin-left: 5px;">Safari</p>
+  </label>
+  </td>
+</tr>
+
+
+<!-- bug text -->
+<tr style="height: 100px" class="bug_allOthers">
+  <td class="text-left">
+  <img src = "pre_CAM/static/browser icons/interview-icon.svg" alt=" " width="50px;" heigth="50px";>
+  OK, now the vital part!
+  <br>
+  <br>
+  <div>
+  Please <b>describe what happened</b>, including any steps you took before you saw the problem. 
+  </div>
+  <p class="text-left small text-muted hide-if-empty" style="margin: 0.25rem 0">
+  </p>
+  </td>
+  <td>
+  <textarea id="bug_text" name="bug_text" class="w-100" rows="8"></textarea>
+  </td>
+</tr>
+
+
+<!-- bug screenshot -->
+<tr style="height: 100px" class="bug_allOthers">
+  <td class="text-left">
+  <img src = "pre_CAM/static/browser icons/screenshot-capture-icon.svg" alt=" " width="50px;" heigth="50px";>
+Screenshots are super helpfull!
+  <br>
+  <br>
+  <div>
+  Do you have a screenshot of the issue you would like to share? 
+  </div>
+  <p class="text-left small text-muted hide-if-empty" style="margin: 0.25rem 0">
+  Darg and Drop image in the gray box (or click on it). A preview of your screenshot will be shown below.
+  </p>
+  </td>
+  <td>
+
+
+<input class="dragBox" type="file" onChange="dragNdrop(event)"  ondragover="drag()" ondrop="drop()" id="uploadFile">
+</input>
+  </td>
+</tr>
+
+
+
+<!-- bug screenshot preview -->
+<tr style="height: 100px" class="bug_allOthers">
+  <td class="text-left" colspan="2">
+  If you have uploaded an image a preview is shown here: 
+  <div id="preview">
+  <i>preview image</i>
+  </div>
+    </td>
+</tr>
+
+
+
+
+<!-- bug code -->
+<tr style="height: 100px" class="bug_allOthers">
+  <td class="text-left">
+  <img src = "pre_CAM/static/browser icons/js-file-icon.svg" alt=" " width="50px;" heigth="50px";>
+Error code is key!
+  <br>
+  <br>
+  <div>
+  Do you have identified any code (e.g., error messages in the Browser console) that could help us to identify the issue?
+  </div>
+  <br>
+  <p class="text-left small text-muted hide-if-empty" style="margin: 0.25rem 0">
+  Just copy and paste the code (with some explenations if needed).
+  </p>
+  </td>
+  <td>
+  <textarea id="bug_code" name="bug_code" class="w-100" rows="8"></textarea>
+  </td>
+</tr>
+
+
 <!-- Column balance -->
 <colgroup>
   <col style="width: 50%">
   <col style="width: 50%">
 </colgroup>
 </table>
+
+
+
+
 
 
 
@@ -104,7 +308,7 @@ report a possible <b>improvement</b>
   For which tool would you like to suggest a possible <b>improvement</b>?
   </td>
   <td>
-    <select id="tool_improvement" name="tool_improvement" required class="w-100">
+    <select id="improvement_tool" name="improvement_tool" class="w-100">
       <option value="" selected>
       -- Please select --
       </option>
@@ -133,7 +337,7 @@ report a possible <b>improvement</b>
   For which <b>element (concept, connection, other)</b> would you like to suggest an improvement?
   </td>
   <td>
-    <select id="improvement_CAMEL_element" name="improvement_CAMEL_element" required class="w-100">
+    <select id="improvement_CAMEL_element" name="improvement_CAMEL_element" class="w-100">
       <option value="" selected>
       -- Please select --
       </option>
@@ -170,7 +374,7 @@ report a possible <b>improvement</b>
     </p>
   </td>
   <td class="center-content">
-  <input type="improvement_CAMEL_importance" name="improvement_CAMEL_importance" min="1" max="9" step="1">
+  <input type="number" name="improvement_CAMEL_importance" min="1" max="9" step="1">
   </td>
 </tr>
 
@@ -190,7 +394,7 @@ report a possible <b>improvement</b>
   For which <b>part (upload, preprocessing, analysis)</b> would you like to suggest an improvement?
   </td>
   <td>
-    <select id="improvement_CAMApp_part" name="improvement_CAMApp_part" required class="w-100">
+    <select id="improvement_CAMApp_part" name="improvement_CAMApp_part" class="w-100">
       <option value="" selected>
       -- Please select --
       </option>
@@ -227,7 +431,7 @@ report a possible <b>improvement</b>
     </p>
   </td>
   <td class="center-content">
-  <input type="improvement_CAMApp_importance" name="improvement_CAMApp_importance" min="1" max="9" step="1">
+  <input type="number" name="improvement_CAMApp_importance" min="1" max="9" step="1">
   </td>
 </tr>
 
@@ -247,7 +451,7 @@ report a possible <b>improvement</b>
   For which <b>part (before log in, after log in, other)</b> of the webpage (administrative panel) would you like to suggest an improvement?
   </td>
   <td>
-    <select id="improvement_Webpage_part" name="improvement_Webpage_part" required class="w-100">
+    <select id="improvement_Webpage_part" name="improvement_Webpage_part" class="w-100">
       <option value="" selected>
       -- Please select --
       </option>
@@ -284,7 +488,7 @@ report a possible <b>improvement</b>
     </p>
   </td>
   <td class="center-content">
-  <input type="improvement_Webpage_importance" name="improvement_Webpage_importance" min="1" max="9" step="1">
+  <input type="number" name="improvement_Webpage_importance" min="1" max="9" step="1">
   </td>
 </tr>
 
@@ -295,7 +499,7 @@ report a possible <b>improvement</b>
 </colgroup>
 </table>
 
-
+</form>
 </div> 
  </main>
  
@@ -308,6 +512,44 @@ report a possible <b>improvement</b>
    </button>
  </footer>
    `,
+   feedbackQues: `
+   <header>
+     <h2>
+    You can leave any comments, thanks for reporting a bug or suggesting an improvement!
+     </h2>
+   </header>
+   
+   <main class="content-horizontal-center content-vertical-center" >
+   <div class="w-l">
+   Just repreat this study if you want to report another bug or suggest another improvement.
+    <br>
+    <br>
+     <form id="page-form" style="display: block;" autocomplete="off">
+ <!-- multiline text text --> 
+ <div class="page-item page-item-textarea" id="page-item-feedback_critic">
+   <p class="text-left font-weight-bold" style="margin: 1rem 0 0.25rem">
+   Do you have any feedback for me to develop the CAM tools? 
+   </p>
+   <p class="text-left small text-muted hide-if-empty" style="margin: 0.25rem 0">
+   Any criticism or suggestions for improvement will be of great help in improving the tools.
+   </p>
+   <textarea name="feedback_critic" class="w-100" rows="4"></textarea>
+ </div>
+ <!-- END multiline text --> 
+      
+     </form>
+   </div> 
+ </main>
+   
+   <footer class="content-vertical-center content-horizontal-right">
+   <button id="continue" type="submit" form="page-form">
+   End study &rarr;
+ </button>
+ </footer>
+   `,
+
+
+
 }
 
 
--- study bug reporting, improvements/pre_CAM/src/js/study.js
+++ study bug reporting, improvements/pre_CAM/src/js/study.js
@@ -1,6 +1,134 @@
 /* 
 ################### introduction phase ###################
 */
+
+/*
+START functions to get browser and operating system information
+*/
+// from: https://stackoverflow.com/questions/5916900/how-can-you-detect-the-version-of-a-browser
+navigator.browserSpecs = (function () {
+  var ua = navigator.userAgent, tem,
+    M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return { name: 'IE', version: (tem[1] || '') };
+  }
+  if (M[1] === 'Chrome') {
+    tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+    if (tem != null) return { name: tem[1].replace('OPR', 'Opera'), version: tem[2] };
+  }
+  M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+  if ((tem = ua.match(/version\/(\d+)/i)) != null)
+    M.splice(1, 1, tem[1]);
+  return { name: M[0], version: M[1] };
+})();
+
+// from: https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browsers
+function getBrowser() {
+  // Opera 8.0+
+  var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+  // Firefox 1.0+
+  var isFirefox = typeof InstallTrigger !== 'undefined';
+
+  // Safari 3.0+ "[object HTMLElementConstructor]" 
+  var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+
+  // Internet Explorer 6-11
+  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+  // Edge 20+
+  var isEdge = !isIE && !!window.StyleMedia;
+
+  // Chrome 1 - 79
+  var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+  // Edge (based on chromium) detection
+  var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+  // Blink engine detection
+  var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+
+  var vec_Browser = ["Opera", "Firefox", "Safari", "IE", "Edge", "Chrome", "Edge Chromium", "Blink"];
+  var vec_Boolean = [isOpera, isFirefox, isSafari, isIE, isEdge, isChrome, isEdgeChromium, isBlink];
+
+
+  if (vec_Boolean.reduce((a, b) => a + b, 0) === 1) {
+    for (i = 0; i < vec_Browser.length; i++) {
+      if (vec_Boolean[i]) {
+        //console.log("Browser detected: ", vec_Browser[i]);
+        return vec_Browser[i];
+      }
+    }
+  }
+}
+
+// from: https://stackoverflow.com/questions/9514179/how-to-find-the-operating-system-details-using-javascript
+navigator.operatingSystem = (function () {
+  var OSName = "Unknown";
+  if (window.navigator.userAgent.indexOf("Windows NT 10.0") != -1) OSName = "Windows 10";
+  if (window.navigator.userAgent.indexOf("Windows NT 6.3") != -1) OSName = "Windows 8.1";
+  if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1) OSName = "Windows 8";
+  if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1) OSName = "Windows 7";
+  if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1) OSName = "Windows Vista";
+  if (window.navigator.userAgent.indexOf("Windows NT 5.1") != -1) OSName = "Windows XP";
+  if (window.navigator.userAgent.indexOf("Windows NT 5.0") != -1) OSName = "Windows 2000";
+  if (window.navigator.userAgent.indexOf("Mac") != -1) OSName = "Mac/iOS";
+  if (window.navigator.userAgent.indexOf("X11") != -1) OSName = "UNIX";
+  if (window.navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+  return OSName;
+})();
+/*
+END functions to get browser and operating system information
+*/
+
+
+/*
+START functions for drag and drop pictures and convert image to Base64
+*/
+// from: https://codepen.io/tusharbandal/pen/YWadbB
+function dragNdrop(event) {
+  var fileName = URL.createObjectURL(event.target.files[0]);
+  var preview = document.getElementById("preview");
+  var previewImg = document.createElement("img");
+  previewImg.setAttribute("src", fileName);
+  preview.innerHTML = "";
+  preview.appendChild(previewImg);
+}
+function drag() {
+  document.getElementById('uploadFile').parentNode.className = 'draging dragBox';
+}
+function drop() {
+  document.getElementById('uploadFile').parentNode.className = 'dragBox';
+}
+
+
+
+
+// global variables
+var Base64Image = undefined;
+
+// from: https://stackoverflow.com/questions/17710147/image-convert-to-base64
+function readFile() {
+  if (!this.files || !this.files[0]) return;
+
+  const FR = new FileReader();
+
+  FR.addEventListener("load", function (evt) {
+    console.log("evt.target.result", evt.target.result)
+    Base64Image = evt.target.result;
+  });
+
+  FR.readAsDataURL(this.files[0]);
+}
+/*
+END functions for drag and drop pictures and convert image to Base64
+*/
+
+
+
+
 const Report_htmlForm = new lab.html.Form({
   title: "Report",
   content: textObj.greetings,
@@ -24,16 +152,25 @@ const Report_htmlForm = new lab.html.Form({
         }
       }
 
+
+      // hide all questions as default (bug report)
+      $("#bug_part_CAMEL").hide()
+      $("#bug_part_CAMApp").hide()
+      $("#bug_part_Webpage").hide()
+
+      $(".bug_allOthers").hide()
+
+
       // show respective questions
       $("#dummy_type, #dummy_type2").on("click", () => {
-        if($('#dummy_type').is(':checked')){
+        if ($('#dummy_type').is(':checked')) {
           $("#type_bug").show();
           $("#type_improvement").hide();
 
           $("#type_improvement_CAMEL").hide();
           $("#type_improvement_CAMApp").hide();
           $("#type_improvement_Webpage").hide();
-        }else{
+        } else {
           $("#type_bug").hide();
           $("#type_improvement").show();
 
@@ -44,9 +181,9 @@ const Report_htmlForm = new lab.html.Form({
       });
 
 
-      $("#tool_improvement").on("input", () => {
-        var tmpValue = $("#tool_improvement option:selected")[0].value;
-       
+      $("#improvement_tool").on("input", () => {
+        var tmpValue = $("#improvement_tool option:selected")[0].value;
+
         if (tmpValue == "CAMEL") {
           $("#type_improvement_CAMEL").show();
 
@@ -59,26 +196,127 @@ const Report_htmlForm = new lab.html.Form({
           $("#type_improvement_Webpage").hide();
         } else if (tmpValue == "Webpage") {
           $("#type_improvement_Webpage").show();
-          
+
           $("#type_improvement_CAMEL").hide();
           $("#type_improvement_CAMApp").hide();
         }
       });
-    
 
-      
+
+      $("#bug_tool").on("input", () => {
+        var tmpValue = $("#bug_tool option:selected")[0].value;
+        $(".bug_allOthers").show()
+        // change attr name
+        // alert("a123")
+
+        $("#placeholder_browser").html(" Did you encounter the issue on <b>this device</b> (" + navigator.operatingSystem + ") using <b>this browser</b> (" + navigator.browserSpecs.name + " " + navigator.browserSpecs.version + ")?");
+        if (tmpValue == "CAMEL") {
+          $("#bug_part_CAMEL").show()
+
+          $("#bug_part_CAMApp").hide()
+          $("#bug_part_Webpage").hide()
+        } else if (tmpValue == "CAMApp") {
+          $("#bug_part_CAMApp").show()
+
+          $("#bug_part_CAMEL").hide()
+          $("#bug_part_Webpage").hide()
+
+        } else if (tmpValue == "Webpage") {
+          $("#bug_part_Webpage").show()
+
+          $("#bug_part_CAMEL").hide()
+          $("#bug_part_CAMApp").hide()
+        }
+      });
+
+
+      // add event listener for convert image to Base64
+      document.querySelector("#uploadFile").addEventListener("change", readFile);
+
+
     },
     commit: () => {
-      // get URL params
       if (typeof jatos.jQuery === "function") {
-        // URLparams_global = jatos.urlQueryParameters;
-        // console.log("URLparams_global:", URLparams_global);
 
+        var array = [];
+        // save all checked browser checkboxes:
+        $(".icon-checkbox:checked").each(function () {
+          // For each checked checkbox, print its id
+          array.push($(this).attr('id'));
+        });
+        console.log("array browsers:", array);
+
+        study.options.datastore.set(
+          "bug_browser_used_additional",
+          array
+        );
+
+
+        // save browser and operating system
+        study.options.datastore.set(
+          "automatic_operatingSystem",
+          navigator.operatingSystem
+        );
+
+        study.options.datastore.set(
+          "automatic_browser",
+          navigator.browserSpecs.name
+        );
+
+        study.options.datastore.set(
+          "automatic_browser2",
+          getBrowser()
+        );
+
+        study.options.datastore.set(
+          "automatic_browserVersion",
+          navigator.browserSpecs.version
+        );
+
+
+        // save base64 image encoding
+        if (Base64Image !== undefined) {
+          study.options.datastore.set(
+            "Base64Image",
+            Base64Image
+          );
+        }
+      }
+    }
+
+  }
+});
+
+
+
+
+// feedback screen
+const FeedbackScreen_htmlScreen = new lab.html.Form({
+  title: "FeedbackScreen",
+  content: textObj.feedbackQues,
+  messageHandlers: {
+    commit: () => {
+    },
+    epilogue: function anonymous() {
+      if (typeof jatos.jQuery === "function") {
+        // If JATOS is available, send data there
+        var resultJson = study.options.datastore.exportJson();
+        console.log("my result data sent to JATOS final time");
+        jatos
+          .submitResultData(resultJson)
+          .then(() => console.log("success"))
+          .catch(() => console.log("error"));
+
+        // redirect
+        jatos.endStudyAndRedirect(
+          "https://drawyourminds.de/", // !!!
+          true,
+          "everything worked fine"
+        );
       }
     },
   },
 });
-
 
 
 // Define the sequence of components that define the study
@@ -97,6 +335,7 @@ const study = new lab.flow.Sequence({
   ],
   content: [
     Report_htmlForm,
+    FeedbackScreen_htmlScreen
   ],
 });
 
